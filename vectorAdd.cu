b465b639b5c6b652b12633146319e73349a912c3
fengjiran <fengjiran@foxmail.com>
2024-09-22 11:22:46 +0800

    add vector add sample

--- vectorAdd.cu
+++ vectorAdd.cu
@@ -1,3 +1,30 @@
 //
 // Created by richard on 9/22/24.
 //
+
+/**
+ * Vector addition: C = A + B.
+ *
+ * This sample is a very basic sample that implements element by element
+ * vector addition. It is the same as the sample illustrating Chapter 2
+ * of the programming guide with some additions like error checking.
+ */
+
+#include <stdio.h>
+
+// For the CUDA runtime routines (prefixed with "cuda_")
+#include <cuda_runtime.h>
+// #include <helper_cuda.h>
+
+/**
+ * CUDA Kernel Device code
+ *
+ * Computes the vector addition of A and B into C. The 3 vectors have the same
+ * number of elements numElements.
+ */
+__global__ void vectorAdd(const float* a, const float* b, float* c, int size) {
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < size) {
+        c[i] = a[i] + b[i] + 0.0f;
+    }
+}
