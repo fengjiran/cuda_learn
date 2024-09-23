b033c326c47d76b8a2c4c25f6322bdd1726b5cc1
fengjiran <fengjiran@foxmail.com>
2024-09-23 13:57:32 +0800

    add helper math functions

--- common/helper_math.h
+++ common/helper_math.h
@@ -57,22 +57,6 @@ inline float rsqrtf(float x) {
 // constructors
 ////////////////////////////////////////////////////////////////////////////////
 
-inline __host__ __device__ float2 make_float2(const float s) {
-    return make_float2(s, s);
-}
-
-inline __host__ __device__ float2 make_float2(const float3 a) {
-    return make_float2(a.x, a.y);
-}
-
-inline __host__ __device__ float2 make_float2(const int2 a) {
-    return make_float2(static_cast<float>(a.x), static_cast<float>(a.y));
-}
-
-inline __host__ __device__ float2 make_float2(const uint2 a) {
-    return make_float2(static_cast<float>(a.x), static_cast<float>(a.y));
-}
-
 inline __host__ __device__ int2 make_int2(const int s) {
     return make_int2(s, s);
 }
@@ -141,10 +125,65 @@ inline __host__ __device__ uint3 make_uint3(const int3 a) {
     return make_uint3(static_cast<uint>(a.x), static_cast<uint>(a.y), static_cast<uint>(a.z));
 }
 
+inline __host__ __device__ int4 make_int4(const int s) {
+    return make_int4(s, s, s, s);
+}
+
+inline __host__ __device__ int4 make_int4(const int3 a) {
+    return make_int4(a.x, a.y, a.z, 0);
+}
+
+inline __host__ __device__ int4 make_int4(const int3 a, const int w) {
+    return make_int4(a.x, a.y, a.z, w);
+}
+
+inline __host__ __device__ int4 make_int4(const uint4 a) {
+    return make_int4(static_cast<int>(a.x), static_cast<int>(a.y),
+                     static_cast<int>(a.z), static_cast<int>(a.w));
+}
+
+inline __host__ __device__ int4 make_int4(const float4 a) {
+    return make_int4(static_cast<int>(a.x), static_cast<int>(a.y),
+                     static_cast<int>(a.z), static_cast<int>(a.w));
+}
+
+inline __host__ __device__ uint4 make_uint4(const uint s) {
+    return make_uint4(s, s, s, s);
+}
+
+inline __host__ __device__ uint4 make_uint4(const uint3 a) {
+    return make_uint4(a.x, a.y, a.z, 0);
+}
+
+inline __host__ __device__ uint4 make_uint4(const uint3 a, const uint w) {
+    return make_uint4(a.x, a.y, a.z, w);
+}
+
+inline __host__ __device__ uint4 make_uint4(const int4 a) {
+    return make_uint4(static_cast<uint>(a.x), static_cast<uint>(a.y),
+                      static_cast<uint>(a.z), static_cast<uint>(a.w));
+}
+
+inline __host__ __device__ float2 make_float2(const float s) {
+    return make_float2(s, s);
+}
+
+inline __host__ __device__ float2 make_float2(const float3 a) {
+    return make_float2(a.x, a.y);
+}
+
+inline __host__ __device__ float2 make_float2(const int2 a) {
+    return make_float2(static_cast<float>(a.x), static_cast<float>(a.y));
+}
+
+inline __host__ __device__ float2 make_float2(const uint2 a) {
+    return make_float2(static_cast<float>(a.x), static_cast<float>(a.y));
+}
+
+
 inline __host__ __device__ float3 make_float3(const float s) {
     return make_float3(s, s, s);
 }
-
 
 inline __host__ __device__ float3 make_float3(const float2 a) {
     return make_float3(a.x, a.y, 0.0f);
@@ -165,5 +204,28 @@ inline __host__ __device__ float3 make_float3(const int3 a) {
 inline __host__ __device__ float3 make_float3(const uint3 a) {
     return make_float3(static_cast<float>(a.x), static_cast<float>(a.y), static_cast<float>(a.z));
 }
+
+inline __host__ __device__ float4 make_float4(const float s) {
+    return make_float4(s, s, s, s);
+}
+
+inline __host__ __device__ float4 make_float4(const float3 a) {
+    return make_float4(a.x, a.y, a.z, 0.0f);
+}
+
+inline __host__ __device__ float4 make_float4(const float3 a, const float w) {
+    return make_float4(a.x, a.y, a.z, w);
+}
+
+inline __host__ __device__ float4 make_float4(const int4 a) {
+    return make_float4(static_cast<float>(a.x), static_cast<float>(a.y),
+                       static_cast<float>(a.z), static_cast<float>(a.w));
+}
+
+inline __host__ __device__ float4 make_float4(const uint4 a) {
+    return make_float4(static_cast<float>(a.x), static_cast<float>(a.y),
+                       static_cast<float>(a.z), static_cast<float>(a.w));
+}
+
 
 #endif//HELPER_MATH_H
